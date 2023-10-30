080dd324ba31f35c6fa159b339d558bf0fdfe19d
LENOVO <LENOVO@DESKTOP-VL4046V>
2023-10-30 22:45:48 +0700

    SECOND commit PROGRAM PERBANDINGAN DUA BUAH NILAI Menambahkan identitas

--- src/projectpbogit24/PROJECTpboGIT24.java
+++ src/projectpbogit24/PROJECTpboGIT24.java
@@ -1,18 +1,13 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+NAMA : FAJAR KRISDIANTORO
+NIM  : 22205004
+PRODI: TI REG SORE
+*/
 package projectpbogit24;
 import java.util.Scanner;
-/**
- *
- * @author LENOVO
- */
-public class PROJECTpboGIT24 {
 
-    /**
-     * @param args the command line arguments
-     */
+public class PROJECTpboGIT24 {
+   
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String jawaban;
